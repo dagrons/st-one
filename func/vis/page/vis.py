8f298dc86657e772aacc1d45f61a97234d5c0cd4
中野三玖 <heyuehuii@126.com>
2024-06-28 20:25:20 +0800

    (func)(vis) update options

--- func/vis/page/vis.py
+++ func/vis/page/vis.py
@@ -8,11 +8,11 @@ from func.vis.page.chart.pyvista_model import vis_pyvista_model
 def vis():
     st.title("可视化")
     placeholder = st.empty()
-    vis_type = st.selectbox("类型", options=['circle', 'map'], label_visibility="collapsed")
     vis_funcs = {
         'circle': vis_circle,
         'map': vis_map,
         'pyvista_model': vis_pyvista_model,
     }
+    vis_type = st.selectbox("类型", options=list(vis_funcs.keys()), label_visibility="collapsed")
     with placeholder.container():
         vis_funcs[vis_type]()
