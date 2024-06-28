6c3b4c06f2925f1aab586383adc53c642e3c02dd
中野三玖 <heyuehuii@126.com>
2024-06-28 20:47:19 +0800

    (refactor)(vis) refactor pyvista_model code

--- func/vis/page/chart/pyvista_model.py
+++ func/vis/page/chart/pyvista_model.py
@@ -9,21 +9,14 @@ def vis_pyvista_model():
     st.info("""Code adapted from https://docs.pyvista.org/user-guide/jupyter/pythreejs.html#scalars-support""")
 
     pv.start_xvfb()
-    ## Initialize a plotter object
     plotter = pv.Plotter(window_size=[400, 400])
-
-    ## Create a mesh with a cube
     mesh = pv.Cube(center=(0, 0, 0))
 
-    ## Add some scalar field associated to the mesh
     mesh['myscalar'] = mesh.points[:, 2] * mesh.points[:, 0]
-
-    ## Add mesh to the plotter
     plotter.add_mesh(mesh, scalars='myscalar', cmap='bwr')
 
-    ## Final touches
     plotter.view_isometric()
     plotter.background_color = 'white'
 
-    ## Send to streamlit
+    # Send to streamlit
     stpyvista(plotter, key="pv_cube")
