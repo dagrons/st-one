206e0b46686c37f10809f62c42ef9ec3adc7fd4d
中野三玖 <heyuehuii@126.com>
2024-06-28 20:50:21 +0800

    (func)(vis) add pyvista_model code

--- func/vis/page/chart/pyvista_model.py
+++ func/vis/page/chart/pyvista_model.py
@@ -5,8 +5,6 @@ import streamlit as st
 
 
 def vis_pyvista_model():
-    st.title("A cube")
-
     pv.start_xvfb()
     plotter = pv.Plotter(window_size=[400, 400])
     mesh = pv.Cube(center=(0, 0, 0))
@@ -19,3 +17,17 @@ def vis_pyvista_model():
 
     # Send to streamlit
     stpyvista(plotter, key="pv_cube")
+    st.code("""
+    pv.start_xvfb()
+    plotter = pv.Plotter(window_size=[400, 400])
+    mesh = pv.Cube(center=(0, 0, 0))
+
+    mesh['myscalar'] = mesh.points[:, 2] * mesh.points[:, 0]
+    plotter.add_mesh(mesh, scalars='myscalar', cmap='bwr')
+    
+    plotter.view_isometric()
+    plotter.background_color = 'white'
+    
+    # Send to streamlit
+    stpyvista(plotter, key="pv_cube")
+    """)
