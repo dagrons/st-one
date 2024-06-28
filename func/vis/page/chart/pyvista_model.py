0954bc076b58624aafa2a5f8f63e304a751776f8
中野三玖 <heyuehuii@126.com>
2024-06-28 20:31:43 +0800

    (func)(vis) start xvfb

--- func/vis/page/chart/pyvista_model.py
+++ func/vis/page/chart/pyvista_model.py
@@ -1,4 +1,5 @@
 import pyvista as pv
+import pyvista.plotting
 from stpyvista import stpyvista
 import streamlit as st
 
@@ -7,6 +8,7 @@ def vis_pyvista_model():
     st.title("A cube")
     st.info("""Code adapted from https://docs.pyvista.org/user-guide/jupyter/pythreejs.html#scalars-support""")
 
+    pyvista.plotting.start_xvfb()
     ## Initialize a plotter object
     plotter = pv.Plotter(window_size=[400, 400])
 
